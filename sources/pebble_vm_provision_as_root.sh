eece1e1bf80b8d8f3fae8dacd6a91fc89f039bef
shalecraig <shalecraig@gmail.com>
2014-03-13 10:16:19 -0700

    Added vim dependency

--- sources/pebble_vm_provision_as_root.sh
+++ sources/pebble_vm_provision_as_root.sh
@@ -16,7 +16,7 @@ cd /home/vagrant
     echo "---------------------------------------------------"
     echo "                     Installing                    "
     echo "---------------------------------------------------"
-    sudo apt-get install --assume-yes wget tar git-core python2.7-dev python-pip python-virtualenv language-pack-en-base
+    sudo apt-get install --assume-yes wget tar git-core python2.7-dev python-pip python-virtualenv language-pack-en-base vim
     echo "---------------------------------------------------"
     echo "               Reconfiguring languages             "
     echo "---------------------------------------------------"
