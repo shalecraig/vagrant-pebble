ade6f2a07d3a5293117c0e80bad074bde66e6869
Seb Flippence <seb@flippence.uk>
2014-11-05 21:14:48 +0000

    Updated to Pebble SDK 2.7 and lastest Ubuntu 14.04 LTS

--- sources/pebble_vm_provision_as_root.sh
+++ sources/pebble_vm_provision_as_root.sh
@@ -1,26 +1,26 @@
-# set -e
-set -x
+#!/bin/bash
+
+echo "---------------------------------------------------"
+echo "                   Start as Root                   "
+echo "---------------------------------------------------"
 cd /home/vagrant
-    echo "---------------------------------------------------"
-    echo "                   Start as Root                   "
-    echo "---------------------------------------------------"
-    echo "---------------------------------------------------"
-    echo "                     Updating                      "
-    echo "---------------------------------------------------"
-    sudo apt-get update
-    # sudo apt-get upgrade, without the grub prompt :s
-    echo "---------------------------------------------------"
-    echo "                     Upgrading                     "
-    echo "---------------------------------------------------"
-    sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes --option Dpkg::Options::="--force-confdef" --option Dpkg::Options::="--force-confold" upgrade
-    echo "---------------------------------------------------"
-    echo "                     Installing                    "
-    echo "---------------------------------------------------"
-    sudo apt-get install --assume-yes wget tar git-core python2.7-dev python-pip python-virtualenv language-pack-en-base vim
-    echo "---------------------------------------------------"
-    echo "               Reconfiguring languages             "
-    echo "---------------------------------------------------"
-    sudo dpkg-reconfigure locales
-    echo "---------------------------------------------------"
-    echo "                    Done as Root                   "
-    echo "---------------------------------------------------"
+echo "---------------------------------------------------"
+echo "                     Updating                      "
+echo "---------------------------------------------------"
+sudo apt-get update
+echo "---------------------------------------------------"
+echo "                     Upgrading                     "
+echo "---------------------------------------------------"
+sudo DEBIAN_FRONTEND=noninteractive apt-get --assume-yes --option Dpkg::Options::="--force-confdef" --option Dpkg::Options::="--force-confold" upgrade
+echo "---------------------------------------------------"
+echo "                     Installing                    "
+echo "---------------------------------------------------"
+sudo apt-get install --assume-yes git-core python-pip python2.7-dev language-pack-en-base
+sudo pip install virtualenv
+echo "---------------------------------------------------"
+echo "               Reconfiguring languages             "
+echo "---------------------------------------------------"
+sudo dpkg-reconfigure locales
+echo "---------------------------------------------------"
+echo "                    Done as Root                   "
+echo "---------------------------------------------------"
