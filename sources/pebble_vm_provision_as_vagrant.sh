16921898025b25d08804e317afa910f57869e372
Wei Cheng <calvinpohwc@gmail.com>
2015-12-28 10:03:46 +0800

    Updated vagrant to provision Pebble Tool instead of SDK. Since SDK3.8 Pebble uses Pebble Tool 4.0 to manage SDKs. There won't be a need to update the repo with each and every version of SDKs released.

--- sources/pebble_vm_provision_as_vagrant.sh
+++ sources/pebble_vm_provision_as_vagrant.sh
@@ -14,21 +14,22 @@ cd /home/vagrant/pebble-dev
 echo "---------------------------------------------------"
 echo "              Getting the pebble sdk               "
 echo "---------------------------------------------------"
-wget -q -a /vagrant/pebble.log http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk2/$PEBBLE_SDK.tar.gz
+#wget -q -a /vagrant/pebble.log http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk2/$PEBBLE_SDK.tar.gz
+wget -q -a /vagrant/peeble.log https://s3.amazonaws.com/assets.getpebble.com/pebble-tool/pebble-sdk-4.0-linux64.tar.bz2
 
-tar -zxf $PEBBLE_SDK.tar.gz
-rm $PEBBLE_SDK.tar.gz
+tar -jxf pebble-sdk-4.0-linux64.tar.bz2
+rm pebble-sdk-4.0-linux64.tar.bz2
 chown -R vagrant:vagrant /home/vagrant/pebble-dev
 echo "---------------------------------------------------"
 echo "                Setting up profile                 "
 echo "---------------------------------------------------"
-echo "export PATH=/home/vagrant/pebble-dev/$PEBBLE_SDK/bin:\$PATH" >> /home/vagrant/.profile
-cd /home/vagrant/pebble-dev/$PEBBLE_SDK
+echo "export PATH=/home/vagrant/pebble-dev/pebble-sdk-4.0-linux64/bin:\$PATH" >> /home/vagrant/.profile
+cd /home/vagrant/pebble-dev/pebble-sdk-4.0-linux64
 echo "---------------------------------------------------"
 echo "               Getting ubuntu tools                "
 echo "---------------------------------------------------"
-wget -q -a /vagrant/pebble.log http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
-tar -zxf arm-cs-tools-ubuntu-universal.tar.gz
+#wget -q -a /vagrant/pebble.log http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/sdk/arm-cs-tools-ubuntu-universal.tar.gz
+#tar -zxf arm-cs-tools-ubuntu-universal.tar.gz
 echo "---------------------------------------------------"
 echo "            Setting up using virtualenv            "
 echo "---------------------------------------------------"
